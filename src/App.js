d491aeee11f2837d1411b3bd107532f386b15c6b
Marcello-Feliciano <165084564+Marcello-Feliciano@users.noreply.github.com>
2025-08-18 18:54:05 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -697,7 +697,7 @@ export default function App() {
                   inset: 0,
                   borderRadius: 8,
                   pointerEvents: "auto",
-                  touchAction: "none", // important for touch-to-drag behavior
+                  touchAction: "pan-x pan-y pinch-zoom", // important for touch-to-drag behavior
                 }}
               >
                 {/* Markers layer */}
